3f9977a3a594dd3a6c54dd649544beca62664a7b
lyumii <dmly1124@gmail.com>
2024-12-05 10:42:37 +0100

    itinial commit

---
+++ const names.js
@@ -0,0 +1,8 @@
+const names = ["Tom", "Eric", "Jessica", "Scott", "Anna", "Carl", "Elisabeth", "Benny", "Oliver", "Andy", "Jenny", "Ashley", "Erin", "Patrick"];
+
+let hello = "";
+
+for (let i = 0; i < names.length; i++) {
+      hello += "Good morning " + names[i] + "\n";
+      console.log(hello);
+      }
